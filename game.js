74f2950a6999ff5246c7e9b093420db31d360dae
Lichessbotacc <110300918+Lichessbotacc@users.noreply.github.com>
2026-05-19 10:33:19 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
+class GameEngine {
 
+  constructor() {
+    this.game = new Chess();
+  }
+
+  move(move) {
+    return this.game.move(move);
+  }
+
+  getFen() {
+    return this.game.fen();
+  }
+
+  moves() {
+    return this.game.moves();
+  }
+
+  isGameOver() {
+    return this.game.game_over();
+  }
+
+  turn() {
+    return this.game.turn();
+  }
+
+}
