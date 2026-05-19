d6434553fdacb6777b2591bfa9be0b4a1fdc9596
Lichessbotacc <110300918+Lichessbotacc@users.noreply.github.com>
2026-05-19 10:34:23 +0200

    Add RandomBot and DarkOnWeakBot classes

--- bots.js
+++ bots.js
@@ -1,1 +1,18 @@
+class RandomBot {
 
+  constructor(name) {
+    this.name = name;
+  }
+
+  getMove(game) {
+    const moves = game.moves();
+    return moves[Math.floor(Math.random() * moves.length)];
+  }
+
+}
+
+class DarkOnWeakBot extends RandomBot {
+  constructor() {
+    super("DarkOnWeakBot");
+  }
+}
